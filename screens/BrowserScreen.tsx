50c0f544f2b71ac9d43afa61d7de8739f8f90c70
bbyik-k <kby00517@naver.com>
2025-06-17 11:49:24 +0900

    feat: add animated loading progress bar to BrowserScreen

--- screens/BrowserScreen.tsx
+++ screens/BrowserScreen.tsx
@@ -1,6 +1,6 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import React, { useMemo, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useMemo, useRef, useState } from 'react';
+import { StyleSheet, Text, View, Animated } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import WebView from 'react-native-webview';
 import { RootStackParamList } from '../routes';
@@ -16,6 +16,8 @@ const style = StyleSheet.create({
     paddingVertical: 5,
   },
   urlText: { fontSize: 12, color: '#f0f0f0' },
+  loadingBarBackground: { height: 3, backgroundColor: '#f0f0f0' },
+  loadingBar: { height: '100%', backgroundColor: 'green', width: '50%' },
 });
 
 export default function BrowserScreen({ route }: Props) {
@@ -26,15 +28,37 @@ export default function BrowserScreen({ route }: Props) {
     [url],
   );
 
+  const progressAnim = useRef(new Animated.Value(0)).current;
+
   return (
     <SafeAreaView style={style.safearea}>
       <View style={style.urlContainer}>
         <Text style={style.urlText}>{urlTitle}</Text>
       </View>
+      <View style={style.loadingBarBackground}>
+        <Animated.View
+          style={[
+            style.loadingBar,
+            {
+              width: progressAnim.interpolate({
+                inputRange: [0, 1],
+                outputRange: ['0%', '100%'],
+              }),
+            },
+          ]}
+        />
+      </View>
       <WebView
         source={{ uri: initialUrl }}
         onNavigationStateChange={event => {
           setUrl(event.url);
+        }}
+        onLoadProgress={event => {
+          // console.log(event.nativeEvent.progress);
+          progressAnim.setValue(event.nativeEvent.progress);
+        }}
+        onLoadEnd={() => {
+          progressAnim.setValue(0);
         }}
       />
     </SafeAreaView>
