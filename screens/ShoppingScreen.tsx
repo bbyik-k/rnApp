17f9b16c1e13ad9fb51bc9f672863b0c6eaba8ff
bbyik-k <kby00517@naver.com>
2025-06-18 13:31:48 +0900

    feat: implement shopping screen with scroll and pull-to-refresh

--- screens/ShoppingScreen.tsx
+++ screens/ShoppingScreen.tsx
@@ -1,24 +1,74 @@
-import React from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
-import { RootStackParamList, RouteNames } from '../routes';
+import React, { useCallback, useRef, useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { RefreshControl, ScrollView, StyleSheet } from 'react-native';
+import WebView from 'react-native-webview';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import { RootStackParamList, RouteNames } from '../routes';
 
 type Props = NativeStackScreenProps<RootStackParamList>;
 
+const style = StyleSheet.create({
+  safearea: { flex: 1 },
+  contentContainerStyle: { flex: 1 },
+});
+
+const START_URI = 'https://shopping.naver.com';
+
 export default function ShoppingScreen({ navigation }: Props) {
+  const webViewRef = useRef<WebView>(null);
+  const [refreshing, setRefreshing] = useState(false);
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    webViewRef.current?.reload();
+  }, []);
+
   return (
-    <View>
-      <TouchableOpacity
-        onPress={() => {
-          navigation.navigate(RouteNames.BROWSER, {
-            initialUrl: 'https://m.naver.com',
-          });
-        }}
+    <SafeAreaView style={style.safearea}>
+      <ScrollView
+        contentContainerStyle={style.contentContainerStyle}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
       >
-        <Text>Go To Browser</Text>
-      </TouchableOpacity>
-      <Icon name="home" size={50} color="#03C75A" />
-    </View>
+        <WebView
+          ref={webViewRef}
+          source={{ uri: START_URI }}
+          showsVerticalScrollIndicator={false}
+          showsHorizontalScrollIndicator={false}
+          onShouldStartLoadWithRequest={request => {
+            console.log(request);
+            if (
+              request.url.startsWith(START_URI) ||
+              request.mainDocumentURL?.startsWith(START_URI)
+            ) {
+              return true;
+            }
+
+            if (request.url != null && request.url.startsWith('https://')) {
+              navigation.navigate(RouteNames.BROWSER, {
+                initialUrl: request.url,
+              });
+              return false;
+            }
+            return true;
+          }}
+          onLoad={() => {
+            setRefreshing(false);
+          }}
+          renderLoading={() => <></>}
+          startInLoadingState={true}
+        />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
+
+/** 
+ * 
+ * Rn Webview 컴포넌트에서 
+
+renderLoading={() => <></>}
+          startInLoadingState={true}
+
+각각의 의미는?
+ */
